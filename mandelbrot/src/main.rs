3868f1d1ce50c8d691cef4e64a482ada9537abe3
Nick Jaremek <nick13jaremek@gmail.com>
2019-06-01 22:18:29 +0200

    Added logic to mandelbrot program to convert pixel to Complex plane point.

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -52,3 +52,32 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<f64>("0.5x",    'x'), None);
     assert_eq!(parse_pair::<f64>("0.5x1.5",    'x'), Some((0.5, 1.5)));
 } 
+
+/// Given the row and column of a pixel in the output image, return the
+/// corresponding point on the complex plane.
+///
+/// `bounds` is a pair giving the width and height of the image in pixels.
+/// `pixel` is a (column, row) pair indicating a particular pixel in that image.
+/// The `upper_left` and `lower_right` parameters are points on the complex
+/// plane designating the are our image covers.
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>)
+    -> Complex<f64> 
+{
+    let (width, height) = (lower_right.re - upper_left.re,
+                           upper_left.im - lower_right.im);
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 100), (25, 75),
+        Complex { re: -1.0, im: 1.0 },
+        Complex { re: 1.0, im: -1.0 }),
+        Complex { re: -0.5, im: -0.5 });
+}
