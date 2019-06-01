8d744b534eccd49b11e073493a487e4b20e2201a
Nick Jaremek <nick13jaremek@gmail.com>
2019-06-01 20:50:17 +0200

    Added command-line logic to hello example which is a greatest common divisor searcher.

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,30 @@
+// These declarations bring into scope the Write and FromStr traits.
+// These allow to use the write and from_str methods
+use std::io::Write;
+use std::str::FromStr;
+
+fn main() {
+    let mut numbers = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+                     .expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}",
+             numbers, d);
+}
+
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0); // This triggers a panic if assertion fails 
     while m != 0 {
