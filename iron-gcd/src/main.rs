899e4ac1b9bb02869bec4fb4544fa31bb629140b
Nick Jaremek <nick13jaremek@gmail.com>
2019-06-01 21:31:42 +0200

    Finished iron-gcd program.

--- iron-gcd/src/main.rs
+++ iron-gcd/src/main.rs
@@ -1,15 +1,25 @@
+extern crate urlencoded;
 extern crate iron;
+extern crate router;
 #[macro_use] extern crate mime; // Use mime crate as well as its defined macros
 
 use iron::prelude::*; // Prelude directives should always be put in scope
 use iron::status;
+use router::Router;
+use std::str::FromStr;
+use urlencoded::UrlEncodedBody;
 
 fn main() {
+    let mut router = Router::new();
+
+    router.get("/", get_form, "root");
+    router.post("/gcd", post_gcd, "gcd");
+
     println!("Serving on http://localhost:3000...");
-    Iron::new(get_form).http("localhost:3000").unwrap();
+    Iron::new(router).http("localhost:3000").unwrap();
 }
 
-// This function is accessed via web
+// This function is accessed via web through GET requests
 fn get_form(_request: &mut Request) -> IronResult<Response> {
     let mut response = Response::new();
 
@@ -18,11 +28,71 @@ fn get_form(_request: &mut Request) -> IronResult<Response> {
     response.set_mut(r#"
     <title>GCD Calculator</title>
     <form action="/gcd" method="post">
-        <input type="text" name="m" />
+        <input type="text" name="n" />
         <input type="text" name="n" />
         <button type="submit">Compute GCD</button>
     </form>
     "#);
 
     Ok(response)
+}
+
+fn post_gcd(request: &mut Request) -> IronResult<Response> {
+    let mut response = Response::new();
+
+    let form_data = match request.get_ref::<UrlEncodedBody>() {
+        Err(e) => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("Error parsing form data: {:?}\n", e));
+            return Ok(response);
+        }
+        Ok(map) => map
+    };
+
+    let unparsed_numbers = match form_data.get("n") {
+        None => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("form data has no 'n' parameter\n"));
+            return Ok(response);
+        }
+        Some(nums) => nums
+    };
+
+    let mut numbers = Vec::new();
+
+    for unparsed in unparsed_numbers {
+        match u64::from_str(&unparsed) {
+            Err(_) => {
+                response.set_mut(status::BadRequest);
+                response.set_mut(
+                    format!("Value for 'n' parameter not a number: {:?}\n", unparsed));
+                return Ok(response);
+            }
+            Ok(n) => { numbers.push(n); }
+        }
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    response.set_mut(status::Ok);
+    response.set_mut(mime!(Text/Html; Charset=Utf8));
+    response.set_mut(
+        format!("The greatest common divisor of the numbers {:?} is <b>{}</b>\n", numbers, d));
+    Ok(response)
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }
