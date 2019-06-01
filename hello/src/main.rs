287e983aee8dbec6bf10e6700f2ee65f6f76bb79
Nick Jaremek <nick13jaremek@gmail.com>
2019-06-01 20:57:51 +0200

    More comments to gcd sample program.

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -4,7 +4,7 @@ use std::io::Write;
 use std::str::FromStr;
 
 fn main() {
-    let mut numbers = Vec::new();
+    let mut numbers: Vec<u64> = Vec::new();
 
     for arg in std::env::args().skip(1) {
         numbers.push(u64::from_str(&arg)
@@ -17,8 +17,9 @@ fn main() {
     }
 
     let mut d = numbers[0];
+    // Borrow items from item 1 to end: get references to items
     for m in &numbers[1..] {
-        d = gcd(d, *m);
+        d = gcd(d, *m); // Deference iterated item with * (star operator)
     }
 
     println!("The greatest common divisor of {:?} is {}",
