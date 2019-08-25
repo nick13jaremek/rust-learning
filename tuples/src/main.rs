f32c810c1a88687fc05c77748a39e44cbd37aded
Nick Jaremek <nick13jaremek@gmail.com>
2019-08-25 17:39:20 +0200

    Added tuples example program.

---
+++ tuples/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let text = "I see the eigenvalue in thine eye";
+    let (head, tail) = text.split_at(21);
+    assert_eq!(head, "I see the eigenvalue ");
+    assert_eq!(tail, "in thine eye");
+}
